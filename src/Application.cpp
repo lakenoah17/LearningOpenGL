b34a1898c9140e0644d05f06a904db2a168a8d37
Noah Shields <noahshields5@gmail.com>
2020-11-01 21:06:06 -0500

    Added the ImGui library and this is getting super cool!!!

--- src/Application.cpp
+++ src/Application.cpp
@@ -13,8 +13,13 @@
 #include "VertexArray.h"
 #include "Shader.h"
 #include "Texture.h"
+
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 int main(void)
 {
@@ -76,15 +81,23 @@ int main(void)
 
         Renderer renderer;
 
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        ImGui::StyleColorsDark();
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
+
+
+        glm::vec3 translation(200, 200, 0);
+
         glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
         glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
-        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
 
-        glm::mat4 mvp = proj * view * model;
 
         Shader shader("res/shaders/Basic.shader");
         shader.Bind();
-        shader.SetUniformMat4f("u_MVP", mvp);
+        
 
         Texture texture("res/textures/destroyer.png");
         texture.Bind();
@@ -105,7 +118,18 @@ int main(void)
             /* Render here */
             renderer.Clear();
 
+            ImGui_ImplOpenGL3_NewFrame();
+
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
+
+
+            glm::mat4 model = glm::translate(glm::mat4(1.0f), translation);
+            glm::mat4 mvp = proj * view * model;
+
+
             shader.Bind();
+            shader.SetUniformMat4f("u_MVP", mvp);
 
             renderer.Draw(va, ib, shader);
 
@@ -115,6 +139,16 @@ int main(void)
             }
             r += increment;
 
+
+            {
+                ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+                ImGui::Text("Application avg %.3f", 1000.0f / ImGui::GetIO().Framerate);
+            }
+
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
             /* Swap front and back buffers */
             GLCall(glfwSwapBuffers(window));
 
@@ -122,6 +156,11 @@ int main(void)
             GLCall(glfwPollEvents());
         }
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+
+    ImGui::DestroyContext();
 
     glfwTerminate();
     return 0;
