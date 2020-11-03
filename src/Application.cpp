c9369da9c3ec91207d3c71f4f35b9585d408691d
Noah Shields <noahshields5@gmail.com>
2020-11-02 20:39:28 -0500

    Added the basics for a test framework to the program removing the jumble of code from the Application file. Tommorow instead of following a video I'm going to implement a bunch of tests for different things like textures

--- src/Application.cpp
+++ src/Application.cpp
@@ -21,6 +21,8 @@
 #include "imgui/imgui_impl_glfw.h"
 #include "imgui/imgui_impl_opengl3.h"
 
+#include "tests/TestClearColor.h"
+
 int main(void)
 {
     GLFWwindow* window;
@@ -54,33 +56,14 @@ int main(void)
     //buffer not being destroyed before glfw is terminated causing the 
     //error checker to loop infinetly because it doesn't have a valid context
     {
-        float positions[] = {
-            -50.0f, -50.0f, 0.0f, 0.0f,
-             50.0f, -50.0f, 1.0f, 0.0f,
-             50.0f,  50.0f, 1.0f, 1.0f,
-            -50.0f,  50.0f, 0.0f, 1.0f
-        };
-
-        unsigned int indicies[] = {
-            0, 1, 2,
-            2, 3, 0
-        };
 
         GLCall(glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA));
         GLCall(glEnable(GL_BLEND));
 
-        VertexArray va;
-        VertexBuffer vb(positions, 4 * 4 * sizeof(float));
-
-        VertexBufferLayout layout;
-        layout.Push<float>(2);
-        layout.Push<float>(2);
-        va.AddBuffer(vb, layout);
-
-        IndexBuffer ib(indicies, 6);
 
         Renderer renderer;
 
+        //Initializes the ImGui library
         IMGUI_CHECKVERSION();
         ImGui::CreateContext();
         ImGuiIO& io = ImGui::GetIO(); (void)io;
@@ -89,84 +72,26 @@ int main(void)
         ImGui_ImplOpenGL3_Init((char*)glGetString(GL_NUM_SHADING_LANGUAGE_VERSIONS));
 
 
-        glm::vec3 translationA(200, 200, 0);
-        glm::vec3 translationB(400, 200, 0);
+        test::TestClearColor test;
 
-        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-
-
-        Shader shader("res/shaders/Basic.shader");
-        shader.Bind();
-        
-
-        Texture texture("res/textures/destroyer.png");
-        texture.Bind();
-        shader.SetUniform1i("u_Texture", 0);
-
-        //Unbinds all elements
-        va.UnBind();
-        shader.UnBind();
-        vb.UnBind();
-        ib.UnBind();
-
-        float r = 0.0f;
-        float increment = .02f;
-
-        /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
-            /* Render here */
             renderer.Clear();
 
-            ImGui_ImplOpenGL3_NewFrame();
+            test.OnUpdate(0.0f);
+            test.OnRender();
 
+            ImGui_ImplOpenGL3_NewFrame();
             ImGui_ImplGlfw_NewFrame();
             ImGui::NewFrame();
 
-
-            //What is entailed in a draw call
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationA);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-
-            //What is entailed in a draw call
-            {
-                glm::mat4 model = glm::translate(glm::mat4(1.0f), translationB);
-                glm::mat4 mvp = proj * view * model;
-                shader.Bind();
-                shader.SetUniformMat4f("u_MVP", mvp);
-
-                renderer.Draw(va, ib, shader);
-            }
-
-            if (r + increment > 1 || r + increment < 0)
-            {
-                increment *= -1;
-            }
-            r += increment;
-
-
-            {
-                ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
-                ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
-                ImGui::Text("Application avg %.3f", 1000.0f / ImGui::GetIO().Framerate);
-            }
-
+            test.OnImGuiRender();
 
             ImGui::Render();
             ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
-            /* Swap front and back buffers */
             GLCall(glfwSwapBuffers(window));
 
-            /* Poll for and process events */
             GLCall(glfwPollEvents());
         }
     }
